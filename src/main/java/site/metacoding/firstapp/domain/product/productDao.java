6c41dff422604b4d57620dd99ac69f579e3d8ebd
bigjang77 <dhex2004@gmail.com>
2022-09-29 16:47:19 +0900

    product DAO 완료

--- src/main/java/site/metacoding/firstapp/domain/product/productDao.java
+++ src/main/java/site/metacoding/firstapp/domain/product/productDao.java
@@ -1,5 +1,11 @@
 package site.metacoding.firstapp.domain.product;
 
-public interface productDao {
+import java.util.List;
 
+public interface productDao {
+    public Product findById(Integer productId);
+    public List<Product> findAll();
+    public void insert(Product product);
+    public void update(Integer producId, Product product);
+    public void deleteById(Integer productId);
 }
